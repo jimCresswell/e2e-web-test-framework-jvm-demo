4cd59024be7d5373d371e38627b8714cce894961
jimCresswell <jim.cresswell@gmail.com>
2019-09-28 18:20:02 +0100

    Add random test data support

--- src/main/java/test_demo/data/TestData.java
+++ src/main/java/test_demo/data/TestData.java
@@ -1,17 +1,59 @@
 package test_demo.data;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Test data generation.
+ */
 public class TestData {
 
-    private static Booking validBooking = new Booking("first name", "last name", "100", true, "2021-01-20", "2021-01-21");
-    private static Booking invalidBooking = new Booking("first name", "last name", "AAA", true, "2021-01-20", "2021-01-21");
-    private static Booking outBeforeInBooking = new Booking("first name", "last name", "100", true, "2021-01-20", "1970-01-01");
-
-    static public Booking getBooking(String bookingType) {
+    /**
+     * Get test data based on a descriptor string e.g. "valid" or "invalid"
+     * @param bookingType the booking type description.
+     * @return a booking data object.
+     * @throws IllegalArgumentException only valid data descriptions are supported.
+     */
+    static public Booking getBooking(@NotNull String bookingType) throws IllegalArgumentException {
         switch (bookingType) {
-            case "valid": return validBooking;
-            case "invalid": return invalidBooking;
-            case "outBeforeIn": return outBeforeInBooking;
+            case "valid": return getValidBooking();
+            case "invalid": return getInvalidBooking();
+            case "outBeforeIn": return getOutBeforeInBooking();
             default: throw new IllegalArgumentException("Unsupported booking type: " + bookingType);
         }
+    }
+
+    /**
+     * Generate a valid booking with a random first name.
+     * @return booking data.
+     */
+    @NotNull
+    private static Booking getValidBooking() {
+        return new Booking("valid_" + generateRandomName(), "last name", "100", true, "2021-01-20", "2021-01-21");
+    }
+
+    /**
+     * Generate an invalid booking with a random first name.
+     * @return booking data.
+     */
+    @NotNull
+    private static Booking getInvalidBooking() {
+        return new Booking("invalid_" + generateRandomName(), "last name", "AAA", true, "2021-01-20", "2021-01-21");
+    }
+
+    /**
+     * Generate a booking with the check-out date before the check-in date.
+     * @return booking data.
+     */
+    @NotNull
+    private static Booking getOutBeforeInBooking() {
+        return new Booking("John", "Smith", "100", true, "2021-01-20", "1970-01-01");
+    }
+
+    @NotNull
+    private static String generateRandomName() {
+        int minLength = 10;
+        int maxLength = 15;
+        return RandomStringUtils.randomAlphabetic(minLength, maxLength + 1);
     }
 }
