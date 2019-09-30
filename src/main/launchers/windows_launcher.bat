5c358888f26a543585101190f067ead4ea3c26fb
jimCresswell <jim.cresswell@gmail.com>
2019-09-30 10:30:52 +0100

    Add a windows specific launch script which sets the chromedriver env variable

---
+++ src/main/launchers/windows_launcher.bat
@@ -0,0 +1,3 @@
+:: Set the runtime environment specific path to the chromedriver instance.
+SET TEST_DEMO_OPTS=-Dwebdriver.chrome.driver="%DIRNAME%..\lib\webdriver\windows\chromedriver.exe"
+test-demo.bat
